6ce6a11b95289f21570b423f0a414ab367f03b65
jotr5402 <jotr5402@colorado.edu>
2025-09-15 19:52:40 -0600

    fix equations for part 2 hopefully

--- task2/arbitrary.m
+++ task2/arbitrary.m
@@ -1,21 +1,21 @@
-function [a] = arbitrary(P, displacement_y)
+function [a_guess] = arbitrary(P, displacement_y)
   L = 4; % m
   E = 6.95*10^10; % Pa
   I = 2.475*10^-6; % m^4
   EI = E*I;
 
-  a = 0;
+  a_guess = 0;
   closest_displacement_difference = Inf;
-  for i = 0 : 0.25 : L
-    if (i < L / 2)
-      y = abs( (1 / EI) * ( ((5 / 48) * (P * i * L^2)) - (i^3 * L / 12) - (P * i * L^2 / 6) + (P * i^3 / 6) ) );
+  for a = 0 : 0.25 : L
+    if (a < L / 2)
+      y = abs( ( (P * a^3 / 12) - (P * a * L^2 / 16) ) / EI );
     else
-      y = abs( (1 / EI) * ( (L^3 * P * (1 - i / L)) - (P * i^3 / 6) + (P * i^2 * L / 4) - (P * i * L^2 / 12) ) );
+      y = abs( ( (P * L^3) - (5 * P * a * L^2) - (8 * P * a^3) + (12 * P * a^2 * L) ) / (48 * EI) );
     end
 
     if ( abs(y - displacement_y) < closest_displacement_difference )
       closest_displacement_difference = abs(y - displacement_y);
-      a = i;
+      a_guess = a;
     end
   end
 
